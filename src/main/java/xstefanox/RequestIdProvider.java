af8db39e500602a60818a6fa7022c585c5457106
Stefano Varesi <stefano.varesi@gmail.com>
2016-05-19 20:30:26 +0200

    store the request ID using Spring RequestContextHolder

--- src/main/java/xstefanox/RequestIdProvider.java
+++ src/main/java/xstefanox/RequestIdProvider.java
@@ -1,24 +1,48 @@
 package xstefanox;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
 
 /**
  * Store the ID of the current request.
  */
 @Service
-public class RequestIdProvider {
+public final class RequestIdProvider {
 
-    private static final ThreadLocal<String> REQUEST_ID = new ThreadLocal<>();
+    /**
+     * The name of the request attribute that holds the request ID in the
+     * {@link org.springframework.web.servlet.support.RequestContext}.
+     */
+    public static final String REQUEST_ATTRIBUTE_NAME = "REQUEST_ID";
 
+    /**
+     * Get the request ID.
+     *
+     * @return The request ID
+     */
     public String get() {
-        return REQUEST_ID.get();
+        return (String) RequestContextHolder.currentRequestAttributes()
+                .getAttribute(REQUEST_ATTRIBUTE_NAME, RequestAttributes.SCOPE_REQUEST);
     }
 
-    public void set(String requestId) {
-        REQUEST_ID.set(requestId);
+    /**
+     * Set the request ID.
+     *
+     * @param requestId The request ID
+     */
+    public void set(final String requestId) {
+        RequestContextHolder
+                .currentRequestAttributes()
+                .setAttribute(REQUEST_ATTRIBUTE_NAME, requestId, RequestAttributes.SCOPE_REQUEST);
     }
 
+    /**
+     * Remove the currently set request ID, if any.
+     */
     public void unset() {
-        REQUEST_ID.remove();
+        RequestContextHolder
+                .currentRequestAttributes()
+                .removeAttribute(REQUEST_ATTRIBUTE_NAME, RequestAttributes.SCOPE_REQUEST);
     }
 }
